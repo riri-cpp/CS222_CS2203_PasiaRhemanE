06ee8f7c0c8138afb61100f74fc95a6a0d20906f
riri-cpp <pasiarheman7@gmail.com>
2026-02-10 14:51:23 +0800

    Wrote a code for calculating the simple interest given the principal amount, rate of interest, and time in years.

--- InterestCalculator/InterestCalculator/InterestCalculator.cs
+++ InterestCalculator/InterestCalculator/InterestCalculator.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.Write("Enter principal amount: ");
+double principal = double.Parse(Console.ReadLine());
+
+Console.Write("Enter rate of interest: ");
+double IntRate = double.Parse(Console.ReadLine());
+
+Console.Write("Enter time in years: ");
+int time = int.Parse(Console.ReadLine());
+
+double SimpleInterest = (principal * IntRate * time) / 100;
+
+Console.WriteLine("Simple Interest: {0:F2}", SimpleInterest);
