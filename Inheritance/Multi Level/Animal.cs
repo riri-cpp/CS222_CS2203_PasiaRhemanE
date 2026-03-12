041e215ad03094e27bdc44bfdfb79040897a3d5d
riri-cpp <pasiarheman7@gmail.com>
2026-03-12 23:36:06 +0800

    multi level

--- Inheritance/Multi Level/Animal.cs
+++ Inheritance/Multi Level/Animal.cs
@@ -1,3 +1,34 @@
 ﻿using System;
 
+Puppy p = new Puppy();
+p.Eat();  // Inherited from Animal
+p.Bark(); // Inherited from Dog
+p.Weep(); // Defined in Puppy
 
+// base class
+class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("The animal is eating");
+    }
+}
+
+// derived from Animal
+class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("The dog is barking");
+    }
+}
+
+// derived from Dog
+class Puppy : Dog
+{
+    public void Weep()
+    {
+        Console.WriteLine("The puppy is weeping");
+    }
+
+}
