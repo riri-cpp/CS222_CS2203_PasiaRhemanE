2e4fd0d9154bbc9b67a4c791726dbafc9e6fe772
riri-cpp <pasiarheman7@gmail.com>
2026-03-12 23:43:27 +0800

    method overriding

--- Inheritance/Method Overriding/Vehicle.cs
+++ Inheritance/Method Overriding/Vehicle.cs
@@ -1,5 +1,12 @@
 ﻿using System;
 
+Vehicle v = new Vehicle();
+v.Move(); // Output: The vehicle is moving
+Car c = new Car();
+c.Move(); // Output: The car is driving
+Motorcycle m = new Motorcycle();
+m.Move(); // Output: The motorcycle is riding
+
 class Vehicle
 {
     public virtual void Move() // virtual method that can be overridden by derived classes
@@ -15,3 +22,11 @@ class Car : Vehicle
         Console.WriteLine("The car is driving");
     }
 }
+
+class Motorcycle : Vehicle
+{
+    public override void Move() // override the Move method to provide specific behavior for Motorcycle
+    {
+        Console.WriteLine("The motorcycle is riding");
+    }
+}   
