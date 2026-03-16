5f9e9413b9c8186a9a18d9383c3c1c1c18ed7877
riri-cpp <pasiarheman7@gmail.com>
2026-03-16 13:52:36 +0800

    unrelated classes inheriting multiple interfaces

---
+++ Abstract Classes and Interfaces/MultipleInterfaces/MultipleInterface.cs
@@ -0,0 +1,52 @@
+﻿SmartPhone sp = new SmartPhone();
+sp.takePhoto();
+sp.playMusic();
+sp.sendMessage();
+
+Console.WriteLine();
+
+Computer pc = new Computer();
+pc.playMusic();
+pc.sendMessage();
+
+interface ICamera
+{
+    void takePhoto();
+}
+
+interface IMusicPlayer
+{
+    void playMusic();
+}
+
+interface IMessenger
+{
+    void sendMessage();
+}
+
+class SmartPhone : ICamera, IMusicPlayer, IMessenger
+{
+    public void takePhoto()
+    {
+        Console.WriteLine("Taking a photo...");
+    }
+    public void playMusic()
+    {
+        Console.WriteLine("Playing music...");
+    }
+    public void sendMessage()
+    {
+        Console.WriteLine("Sending a message...");
+    }
+}
+class Computer : IMusicPlayer, IMessenger
+{
+    public void playMusic()
+    {
+        Console.WriteLine("Playing music on computer...");
+    }
+    public void sendMessage()
+    {
+        Console.WriteLine("Sending a message from computer...");
+    }
+}
